d732a0d7474a7fd0464f520acf919c3cd0fb859c
Dave Grijalva <grijalva@gmail.com>
2014-01-27 18:20:16 -0800

    getting somehwere

--- parser.go
+++ parser.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"encoding/binary"
 	"fmt"
+	"math"
+	"reflect"
 )
 
 func Parse(rdr io.Reader)([]Atom, error) {
@@ -13,40 +15,60 @@ func Parse(rdr io.Reader)([]Atom, error) {
 	var a Atom
 	for err == nil {
 		if a, err = parseNext(rdr); err == nil {
-			fmt.Println(a.(*FileTypeAtom).Length, string(a.(*FileTypeAtom).Type))
+			fmt.Println(a, a.Length(), string(a.Type()))
 			atoms = append(atoms, a)
 		}
-	}
-	if err == io.EOF {
-		err = nil
 	}
 	return atoms, err
 }
 
 func parseNext(rdr io.Reader)(Atom, error) {
-	if size, typ, i, err := readAtomHeader(rdr); err == nil {
-		a := &FileTypeAtom{size, typ}
-		// the last atom has a size of 0
-		if size == 0 {
-			io.Copy(ioutil.Discard, rdr)
+	if a, i, err := readAtomHeader(rdr); err == nil {
+		// Limit the rest of the reads
+		if a.Length() == 0 {
+			rdr = &io.LimitedReader{rdr, math.MaxInt64}
 		} else {
-			b := make([]byte, size - uint64(i))
-			_, err = rdr.Read(b)
+			rdr = &io.LimitedReader{rdr, int64(a.Length() - uint64(i))}
 		}
-		return a, err
+		
+		// Find out if atom is of a known type
+		atom := upgradeType(a)
+
+		// Parse special headers if there are any
+		// fmt.Println(rdr.(*io.LimitedReader).N)
+		if err = parseSpecialHeaders(rdr, atom); err != nil {
+			if err != io.EOF {
+				return atom, err
+			}
+		}
+
+		// Handle the remaining data.  An atom either has
+		// child atoms or data, never both
+		if !atom.Leaf() {
+			if c, err := Parse(rdr); err == nil {
+				a.ChildAtoms = c
+			}
+		}
+		
+		// Discard remaining data
+		// FIXME: we need this data eventually
+		io.Copy(ioutil.Discard, rdr)
+		
+		return atom, err
 	} else {
 		return nil, err
 	}
 }
 
-func readAtomHeader(rdr io.Reader)(size uint64, typ []byte, bytesRead int, err error){
+func readAtomHeader(rdr io.Reader)(atom *BasicAtom, bytesRead int, err error){
+	atom = &BasicAtom{}
+	atom.typ = make([]byte, 4)
 	var sizeBlock = make([]byte, 8)
-	typ = make([]byte, 4)
 	var readExtendedSize = false
 	// Read simple size
 	if _, err = rdr.Read(sizeBlock[0:4]); err == nil {
-		size = uint64(binary.BigEndian.Uint32(sizeBlock[0:4]))
-		if size == 1 {
+		atom.length = uint64(binary.BigEndian.Uint32(sizeBlock[0:4]))
+		if atom.length == 1 {
 			readExtendedSize = true
 		}
 	} else {
@@ -55,7 +77,7 @@ func readAtomHeader(rdr io.Reader)(size uint64, typ []byte, bytesRead int, err e
 	bytesRead += 4
 
 	// Read type header
-	if _, err = rdr.Read(typ); err != nil {
+	if _, err = rdr.Read(atom.typ); err != nil {
 		return
 	}
 	bytesRead += 4
@@ -63,12 +85,50 @@ func readAtomHeader(rdr io.Reader)(size uint64, typ []byte, bytesRead int, err e
 	// Read extended size
 	if readExtendedSize {
 		if _, err = rdr.Read(sizeBlock); err == nil {
-			size = binary.BigEndian.Uint64(sizeBlock)
+			atom.length = binary.BigEndian.Uint64(sizeBlock)
 		} else {
 			return
 		}
+		bytesRead += 8
 	}
-	bytesRead += 8
 	
 	return
+}
+
+func upgradeType(b *BasicAtom)Atom {
+	switch(string(b.Type())){
+	case "ftyp":
+		return &FileTypeAtom{BasicAtom: b}
+	default:
+		return b
+	}
+}
+
+func parseSpecialHeaders(rdr io.Reader, atom Atom)error {
+	value := reflect.ValueOf(atom)
+	writeValue := value.Elem()
+	if value.Kind() == reflect.Ptr {
+		value = reflect.Indirect(value)
+	}
+	if value.Kind() == reflect.Struct {
+		var i = 0
+		for n := value.NumField(); i < n; i++ {
+			field := writeValue.Field(i)
+			fieldDesc := value.Type().Field(i)
+			if tag := fieldDesc.Tag.Get("qtff"); tag != "" {
+				readBlock := make([]byte, 16)
+				// fmt.Println(field)
+				switch field.Kind() {
+				case reflect.Uint32:
+					if _, err := rdr.Read(readBlock[0:4]); err == nil {
+						field.Set(reflect.ValueOf(binary.BigEndian.Uint32(readBlock[0:4])))
+						// fmt.Println(atom, field)
+					} else {
+						// return err
+					}
+				}
+			}
+		}
+	}
+	return nil
 }
--- types.go
+++ types.go
@@ -1,9 +1,40 @@
 package qtff
 
 type Atom interface {
+	// returns the type of atom
+	Type()[]byte
+	// returns true if the atom is a leaf node
+	Leaf()bool
+	// length of encoded atom
+	Length()uint64
+	// Child atoms
+	Children()[]Atom
+}
+
+type BasicAtom struct {
+	length uint64
+	typ []byte
+	ChildAtoms []Atom
+}
+
+func (a *BasicAtom) Type()[]byte {
+	return a.typ
+}
+
+func (a *BasicAtom) Leaf()bool {
+	return true
+}
+
+func (a *BasicAtom) Length()uint64 {
+	return a.length
+}
+
+func (a *BasicAtom) Children()[]Atom {
+	return a.ChildAtoms
 }
 
 type FileTypeAtom struct {
-	Length uint64
-	Type []byte
+	*BasicAtom
+	MajorBrand uint32 `qtff:"0"`
+	MinorVersion uint32 `qtff:"1"`
 }
